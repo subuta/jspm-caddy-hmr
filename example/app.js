9213b9172e6c8113887867f4960bd1d6463ecedd
shunta saito <s.shunta@goodpatch.com>
2016-06-04 13:31:37 +0900

    [modify]implement hmr.

---
+++ example/app.js
@@ -0,0 +1,11 @@
+import _ from 'lodash';
+
+// js example.
+import example from 'example/nested/example.js';
+// css example.
+import 'example/nested/sample.css!';
+
+console.log('app loaded!');
+
+// call js.
+example();
